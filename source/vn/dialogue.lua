5b0f0411b51210d52bfc8e8f9c7e4e2f2bddbbc1
Bigaston <bigaston1@gmail.com>
2022-11-19 12:34:26 +0100

    Move animate text function

--- source/vn/dialogue.lua
+++ source/vn/dialogue.lua
@@ -4,46 +4,60 @@ local pd <const> = playdate
 local gfx <const> = pd.graphics
 
 local openingTime <const> = 500
-local frameBetweenText <const> = 5
+local frameBetweenText <const> = 3
 
 -- Class that displays dialogue box
 class('Dialogue').extends()
 
+-- Init the dialogue class
 function Dialogue:init(text, opened) 
   Dialogue.super.init(self)
   self.text = text
-  self.opened = opened
 
   self.textAnimation = false
+  self.textDisplay = false
   self.textElapsedFrame = 0
   self.textDisplayChar = 0
 
+  self.opened = opened
   self.openAnimation = false
 end
 
+-- Ask for the game to open dialogue box and start animation
 function Dialogue:open()
   self.timerX = pd.timer.new(openingTime, 0, 196, pd.easingFunctions.inOutSine)
   self.timerY = pd.timer.new(openingTime, 0, 38, pd.easingFunctions.inOutSine)
   self.opened = false
-  self.textAnimation = false
   self.openAnimation = true
+end
+
+function Dialogue:animateText()
+  self.textElapsedFrame = frameBetweenText
+  self.textDisplayChar = 0
+  self.textDisplay = false
+  self.textAnimation = true
 end
 
 function Dialogue:update()
   if self.openAnimation then
     if not self.timerX.active then
-      self.opened = true
-      self.textAnimation = true
-      self.textElapsedFrame = frameBetweenText
-      
+      self.opened = true     
       self.openAnimation = false
+
+      self:animateText()
     end
   end
 
   if self.textAnimation then
     if self.textElapsedFrame == 0 then
       self.textElapsedFrame = frameBetweenText
-      self.textDisplayChar +=1 
+      self.textDisplayChar +=1
+
+      if self.textDisplayChar == #self.text then
+        self.textAnimation = false
+        self.textDisplay = true
+      end
+
     else 
       self.textElapsedFrame-=1
     end
@@ -65,6 +79,9 @@ function Dialogue:draw()
 
   if self.textAnimation then
     gfx.drawText(string.sub(self.text, 0, self.textDisplayChar), 5, 163)
+  elseif self.textDisplay then
+    gfx.drawText(self.text, 5, 163)
+
   end
   -- gfx.drawText(self.timerX.value, 10, 10)
 end
