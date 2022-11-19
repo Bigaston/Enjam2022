edfedcdf729182ca2eb3d39d515557873a9cc77a
Dorian Vabre <dorian.vabre@gmail.com>
2022-11-19 13:44:09 +0100

    movement OK, border collision OK

--- source/game/game.lua
+++ source/game/game.lua
@@ -3,14 +3,36 @@ import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 
+import "game/player"
+
 local gfx <const> = playdate.graphics
 
+playerSpriteSize = 32
+borderSize = 2
+minimumZoneX = borderSize
+maximumZoneX = 400 - borderSize
+minimumZoneY = borderSize
+maximumZoneY = 240 - borderSize
+
 function initializeGame()
-    
+
+    -- Init player instance
+    local playerImage = gfx.image.new("images/game/rightPlayerPlusGros")
+    local playerInstance = Player(200, 120, playerImage)
+    playerInstance:add()
+
+    -- Init border
+    local borderImage = gfx.image.new("images/game/border")
+    gfx.sprite.setBackgroundDrawingCallback(
+        function(x, y, width, height)
+            gfx.setClipRect(x, y, width, height)
+            borderImage:draw(0, 0)
+        end
+    )
 end
 
 function drawGame()
-    
+    gfx.sprite.update()
 end
 
 function updateGame()
---
+++ source/game/player.lua
@@ -0,0 +1,68 @@
+local pd <const> = playdate
+local gfx <const> = pd.graphics
+
+class("Player").extends(gfx.sprite)
+
+function Player:init(x, y, image)
+    self.x = x
+    self.y = y
+    self:moveTo(self.x, self.y)
+    self.currentImage = image
+    self.currentRotation = 0
+    self.rotationModifier = 5
+    self:setImage(image)
+end
+
+function Player:manageRotation()
+    if pd.buttonIsPressed(pd.kButtonRight) then
+        self.currentRotation -= self.rotationModifier
+        if self.currentRotation <= 0 then
+            self.currentRotation += 360
+        end
+        self:setImage(self.currentImage:rotatedImage(360 - self.currentRotation))
+    end
+    if pd.buttonIsPressed(pd.kButtonLeft) then
+        self.currentRotation += self.rotationModifier
+        if self.currentRotation >= 360 then
+            self.currentRotation -= 360
+        end
+        self:setImage(self.currentImage:rotatedImage(360 - self.currentRotation))
+    end
+end
+
+-- Checks the crank to move forward or backward
+function Player:manageMovement()
+    local change, acceleratedChange = playdate.getCrankChange()
+    local x = 0
+    local y = 0
+    speed = change/5
+
+    if (self.currentRotation == 90 or currentRotation == 270) then
+        -- the value of cos is too low and math.floor won't work
+        x = self.x
+    else   
+        x = self.x + speed * math.cos(self.currentRotation * math.pi / 180)
+    end
+
+    if (self.currentRotation == 0 or currentRotation == 180) then
+        -- the value of sin is too low and math.floor won't work
+        y = self.y
+    else   
+        y = self.y - speed * math.sin(self.currentRotation * math.pi / 180)
+    end
+    
+    -- Check if the player will be out of bounds, don't move if true
+    if (x - playerSpriteSize/2 > minimumZoneX and x + playerSpriteSize/2 < maximumZoneX 
+        and y - playerSpriteSize/2 > minimumZoneY and y + playerSpriteSize/2 < maximumZoneY) then
+        self.x = x
+        self.y = y
+        self:moveTo(self.x, self.y)
+    end
+
+end
+
+function Player:update()
+    Player.super.update(self)
+    self:manageRotation()
+    self:manageMovement()
+end
