50e1b245e2336c09e6eb1dba0415f361710a80ee
Dorian Vabre <dorian.vabre@gmail.com>
2022-11-20 10:07:21 +0100

    cultists now fleeing away from the player, still needs a timer to prevent transitions too fast

--- source/game/cultist.lua
+++ source/game/cultist.lua
@@ -1,3 +1,5 @@
+import "game/game"
+
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
@@ -16,6 +18,7 @@ function Cultist:init(x, y, image)
     self.targetLocationX = 0
     self.targetLocationY = 0
     self.isScared = false
+    self.distanceToBeScared = 60
     self.speed = 1
     self.maxDeltaForRandomMovements = 10
     self:setCollideRect(0, 0, self:getSize())
@@ -23,8 +26,14 @@ function Cultist:init(x, y, image)
 end
 
 function Cultist:update()
+
+    -- Checking if the cultist is scared
+    local playerX, playerY = getPlayerPosition()
+    self.isScared = (math.sqrt((playerX-self.x)^2 + (playerY-self.y)^2) <= self.distanceToBeScared)
+
     if self.isScared then
         -- Cultist scared, fleeing away from the player
+        self:runAway(playerX, playerY)
         -- TODO
     elseif (self.hasReachedLocation and not self.isScared) then
         -- Cultist not scared, looking for a new location to roam
@@ -35,6 +44,7 @@ function Cultist:update()
     end
 end
 
+-- Generates a random location for the cultist to roam to, by changing x OR y by a maximum value of self.maxDeltaForRandomMovements
 function Cultist:generateRoamLocation()
     self.hasReachedLocation = false
     local changeXorY = math.random(2)
@@ -47,24 +57,43 @@ function Cultist:generateRoamLocation()
     end
 end
 
+-- Run away from the player
+function Cultist:runAway(playerX, playerY)
+    local directionToFleeX = self.x - playerX
+    local directionToFleeY = self.y - playerY
+
+    local normalizedX = 0
+    local normalizedY = 0
+
+    -- Normalize x and y
+    if (directionToFleeX ~= 0) then
+        normalizedX = (directionToFleeX)/math.abs(directionToFleeX)
+    end
+    if (directionToFleeY ~= 0) then
+        normalizedY = (directionToFleeY)/math.abs(directionToFleeY)
+    end
+
+    self:moveBy(normalizedX * self.speed, normalizedY * self.speed)
+end
+
 function Cultist:roamToTarget()
     local normalizedX = 0
-        local normalizedY = 0
+    local normalizedY = 0
 
-        -- Normalize x and y
-        if (self.targetLocationX-self.x ~= 0) then
-            normalizedX = (self.targetLocationX-self.x)/math.abs(self.targetLocationX-self.x)
-        elseif (self.targetLocationY-self.y ~= 0) then
-            normalizedY = (self.targetLocationY-self.y)/math.abs(self.targetLocationY-self.y)
-        end
+    -- Normalize x and y
+    if (self.targetLocationX-self.x ~= 0) then
+        normalizedX = (self.targetLocationX-self.x)/math.abs(self.targetLocationX-self.x)
+    elseif (self.targetLocationY-self.y ~= 0) then
+        normalizedY = (self.targetLocationY-self.y)/math.abs(self.targetLocationY-self.y)
+    end
 
-        -- Apply the movespeed to the normalized vector on x or y
-        self:moveBy(normalizedX * self.speed, normalizedY * self.speed)
+    -- Apply the movespeed to the normalized vector on x or y
+    self:moveBy(normalizedX * self.speed, normalizedY * self.speed)
 
-        -- If close enough 
-        if math.abs(self.targetLocationX-self.x) < self.speed and math.abs(self.targetLocationY-self.y) < self.speed then
-            self.hasReachedLocation = true
-        end
+    -- If close enough 
+    if math.abs(self.targetLocationX-self.x) < self.speed and math.abs(self.targetLocationY-self.y) < self.speed then
+        self.hasReachedLocation = true
+    end
 end
 
 -- Careful! self.x has to already be in bound or infinite loop
