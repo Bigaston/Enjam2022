73d37db576512101f4ffff979ffce6e66d865c7b
Bigaston <bigaston1@gmail.com>
2022-11-20 12:05:06 +0100

    Changement du menu

--- source/menu.lua
+++ source/menu.lua
@@ -5,21 +5,28 @@ import "language"
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
-local ui
-local uiText <const> = {"menu.level", "menu.lang"}
+local menuUi
+local menuUiText <const> = {"menu.level", "menu.lang"}
 local langUi = 2
+
+local levelUi
+local levelUiText <const> = {"menu.level1", "menu.level2", "menu.level3", "menu.level4"}
+
+local menuScreen = "base"
+local menuChangeTimer
+local menuDifferentY = 0
 
 local selectorImg = gfx.image.new("images/menu/selector")
 local leftRightImg = gfx.image.new("images/menu/leftright")
 
 function initMenu()
-  ui = pd.ui.gridview.new(350, 32)
+  menuUi = pd.ui.gridview.new(350, 32)
 
-  ui:setNumberOfRows(2)
-  ui:setNumberOfColumns(1)
-  ui:setCellPadding(0, 0, 0, 10)
+  menuUi:setNumberOfRows(2)
+  menuUi:setNumberOfColumns(1)
+  menuUi:setCellPadding(0, 0, 0, 10)
 
-  function ui:drawCell(section, row, column, selected, x, y, width, height)
+  function menuUi:drawCell(section, row, column, selected, x, y, width, height)
     if selected then
       selectorImg:draw(x, y)
     end
@@ -27,32 +34,78 @@ function initMenu()
     gfx.drawRoundRect(x + 40, y, width-40, height, 5)
 
     if row ~= langUi then
-      gfx.drawText(Language.getString(uiText[row]), x + 45, y + 7)
+      gfx.drawText(Language.getString(menuUiText[row]), x + 45, y + 7)
     else
-      gfx.drawText(Language.getString(uiText[row]) .. " " .. Language.getDisplayLang(), x + 45, y + 7)
+      gfx.drawText(Language.getString(menuUiText[row]) .. " " .. Language.getDisplayLang(), x + 45, y + 7)
       leftRightImg:draw(x + width - 30, y)
     end
+  end
+
+  levelUi = pd.ui.gridview.new(350, 32)
+  levelUi:setNumberOfRows(4)
+  levelUi:setNumberOfColumns(1)
+  levelUi:setCellPadding(0, 0, 0, 10)
+
+  function levelUi:drawCell(section, row, column, selected, x, y, width, height)
+    if selected then
+      selectorImg:draw(x, y)
+    end
+
+    gfx.drawRoundRect(x + 40, y, width-40, height, 5)
+
+    gfx.drawText(Language.getString(levelUiText[row]), x + 45, y + 7)
   end
 end
 
 function updateMenu()
-  if pd.buttonJustPressed(pd.kButtonDown) then
-    ui:selectNextRow(true)
-  elseif pd.buttonJustPressed(pd.kButtonUp) then
-    ui:selectPreviousRow(true)
+  if menuChangeTimer ~= nil and menuChangeTimer.active then
+    menuDifferentY = menuChangeTimer.value
   end
 
-  if ui:getSelectedRow() == langUi then
-    if pd.buttonJustPressed(pd.kButtonRight) or pd.buttonJustPressed(pd.kButtonLeft) then
-      if Language.getLang() == "fr" then
-        Language.setLang("en")
-      else
-        Language.setLang("fr")
+  if menuScreen == "base" then
+    if pd.buttonJustPressed(pd.kButtonDown) then
+      menuUi:selectNextRow(true)
+    elseif pd.buttonJustPressed(pd.kButtonUp) then
+      menuUi:selectPreviousRow(true)
+    end
+
+    if menuUi:getSelectedRow() == 1 and pd.buttonIsPressed(pd.kButtonA) then
+      menuChangeTimer = pd.timer.new(1000, 0, -400, pd.easingFunctions.inOutCubic)
+      levelUi:setSelectedRow(1)
+
+      function menuChangeTimer:timerEndedCallback()
+        menuScreen = "level"
+      end
+    end
+
+    if menuUi:getSelectedRow() == langUi then
+      if pd.buttonJustPressed(pd.kButtonRight) or pd.buttonJustPressed(pd.kButtonLeft) then
+        if Language.getLang() == "fr" then
+          Language.setLang("en")
+        else
+          Language.setLang("fr")
+        end
+      end
+    end
+  elseif menuScreen == "level" then
+    if pd.buttonJustPressed(pd.kButtonDown) then
+      levelUi:selectNextRow(true)
+    elseif pd.buttonJustPressed(pd.kButtonUp) then
+      levelUi:selectPreviousRow(true)
+    end
+
+    if pd.buttonJustPressed(pd.kButtonB) then
+      menuChangeTimer = pd.timer.new(1000, -400, 0, pd.easingFunctions.inOutCubic)
+      menuUi:setSelectedRow(1)
+
+      function menuChangeTimer:timerEndedCallback()
+        menuScreen = "base"
       end
     end
   end
 end
 
 function drawMenu()
-  ui:drawInRect(10, 10, 390, 190)
+  menuUi:drawInRect(menuDifferentY + 10, 10, 390, 190)
+  levelUi:drawInRect(menuDifferentY + 410, 10, 390, 190)
 end
